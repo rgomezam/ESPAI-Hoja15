1043a61973e134218c0eb2a410db68eea78ddfff
Raquel Gomez <raquel.gomez@cern.ch>
2015-08-20 14:39:00 +0200

    Create Reparacion.cpp

---
+++ ejercicio7/Reparacion.cpp
@@ -0,0 +1,19 @@
+/*
+ * Reparacion.cpp
+ *
+ *  Created on: Aug 20, 2015
+ *      Author: raquel
+ */
+
+#include "Reparacion.h"
+
+
+Reparacion::Reparacion(string nombreRep) {
+	this->nombreRep=nombreRep;
+	cout << "Creando reparacion " << nombreRep << endl;
+}
+
+Reparacion::~Reparacion() {
+	// TODO Auto-generated destructor stub
+}
+
